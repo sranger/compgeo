e3bcd3d986f2d8bd0f2d81c67f276e9ebab740b6
rangers <rangers@9335714d-51df-4f03-a7f8-1812403f7df7>
2015-02-09 20:45:36 +0000

    rearranged code to fit into ant build from animationEngine project, modified output.txt to contain the algorithm type, and added build and run scripts

---
+++ run_assignment1_bruteforce.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+
+##
+# Java VM flags
+JAVA_FLAGS='-Djava.net.useSystemProxies=true -Xmx2048m'
+
+##
+# The main class
+MAIN='com.stephenwranger.compgeo.assignment1.Assignment1'
+
+EXTRA_CLASSPATH='';
+
+for JAR in $( find ./lib -name '*.jar' )
+do
+  EXTRA_CLASSPATH=$EXTRA_CLASSPATH:$JAR
+done
+
+for JAR in $( find ./dist -name '*.jar' )
+do
+  EXTRA_CLASSPATH=$EXTRA_CLASSPATH:$JAR
+done
+
+if [ -n "$EXTRA_CLASSPATH" ]; then
+   if [ -n "$CLASSPATH" ]; then
+     export CLASSPATH="$CLASSPATH:$EXTRA_CLASSPATH"
+   else
+     export CLASSPATH="$EXTRA_CLASSPATH"
+   fi
+fi
+
+export CLASSPATH
+
+java ${JAVA_FLAGS} ${MAIN} BruteForce $@
+
