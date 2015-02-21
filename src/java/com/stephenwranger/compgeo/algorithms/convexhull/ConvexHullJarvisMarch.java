bcd7d5a92b547226460947135674bdea0d15c030
Stephen <stephen.w.ranger@gmail.com>
2015-02-21 03:37:01 +0000

    updated jarvis to use points directly instead of indices

--- src/java/com/stephenwranger/compgeo/algorithms/convexhull/ConvexHullJarvisMarch.java
+++ src/java/com/stephenwranger/compgeo/algorithms/convexhull/ConvexHullJarvisMarch.java
@@ -1,6 +1,6 @@
 package com.stephenwranger.compgeo.algorithms.convexhull;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -22,57 +22,55 @@ public class ConvexHullJarvisMarch implements Algorithm<Tuple2d> {
 
       final long startTime = System.nanoTime();
 
-      int lowest = 0;
+      Tuple2d lowest = input.get(0);
 
-      for (int i = 1; i < input.size(); i++) {
-         if (input.get(i).x < input.get(lowest).x) {
-            lowest = i;
-         } else if (input.get(i).x == input.get(lowest).x && input.get(i).y < input.get(lowest).y) {
-            lowest = i;
+      for (final Tuple2d point : input) {
+         if (point.x < lowest.x) {
+            lowest = point;
+         } else if (point.x == lowest.x && point.y < lowest.y) {
+            lowest = point;
          }
       }
 
-      final int[] next = new int[input.size()];
-      Arrays.fill(next, -1);
-
-      int i = 0;
-      next[0] = lowest;
+      final List<Tuple2d> results = new ArrayList<Tuple2d>();
+      Tuple2d next = lowest;
+      Tuple2d previous = null;
 
       try {
          do {
+            results.add(next);
+            previous = next;
             if ((System.nanoTime() - startTime) / 1000000l > timeout) {
                return false;
             }
 
-            next[i + 1] = ConvexHullJarvisMarch.nextPoint(input, next[i]);
-            i++;
-         } while (next[i] != next[0]);
+            if (results.size() > input.size()) {
+               throw new Exception("results > input");
+            }
+
+            next = ConvexHullJarvisMarch.nextPoint(input, previous);
+         } while (next != lowest);
       } catch (final Exception e) {
          e.printStackTrace();
-         for (final int index : next) {
-            if (index != -1) {
-               System.err.println(index + ", " + input.get(index));
-            }
+         for (final Tuple2d point : results) {
+            System.err.println(point);
          }
 
+         int ctr = 0;
          for (final Tuple2d t : input) {
-            System.err.println(t);
+            System.err.println(ctr + ": " + t);
+            ctr++;
          }
 
          return false;
       }
 
-      Tuple2d point;
-
-      for (final int index : next) {
-         if (index != -1) {
-            point = input.get(index);
-            minX = Math.min(minX, point.x);
-            maxX = Math.max(maxX, point.x);
-            minY = Math.min(minY, point.y);
-            maxY = Math.max(maxY, point.y);
-            output.add(input.get(index));
-         }
+      for (final Tuple2d point : results) {
+         minX = Math.min(minX, point.x);
+         maxX = Math.max(maxX, point.x);
+         minY = Math.min(minY, point.y);
+         maxY = Math.max(maxY, point.y);
+         output.add(point);
       }
 
       final double centerX = (maxX - minX) / 2.0 + minX;
@@ -82,15 +80,15 @@ public class ConvexHullJarvisMarch implements Algorithm<Tuple2d> {
       return true;
    }
 
-   public static int nextPoint(final List<Tuple2d> input, final int p) {
-      int q = p;
+   public static Tuple2d nextPoint(final List<Tuple2d> input, final Tuple2d p) {
+      Tuple2d q = p;
       int turn;
 
-      for (int r = 0; r < input.size(); r++) {
+      for (final Tuple2d r : input) {
          if (r != q) {
-            turn = ConvexHullJarvisMarch.turn(input.get(p), input.get(r), input.get(q));
+            turn = ConvexHullJarvisMarch.turn(p, r, q);
 
-            if (turn == -1 || (turn == 0 && ConvexHullJarvisMarch.dist(input, p, r) > ConvexHullJarvisMarch.dist(input, p, q))) {
+            if (turn == -1 || (turn == 0 && ConvexHullJarvisMarch.dist(p, r) > ConvexHullJarvisMarch.dist(p, q))) {
                q = r;
             }
          }
